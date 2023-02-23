25fb5bdd96a68201534d7d66fb1f8368b27613b4
Rui Dantas <ruidantas55@gmail.com>
2023-02-23 11:34:38 -0500

    Show an item from Day List, Write a function to show information 1 spot and 0 spots, Add CSS

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames";
+
+
+
+import "components/DayListItem.scss"
+
+
+
+export default function DayListItem(props) {
+  const FormatSpots = () => {
+    if(props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>
+    }
+    if(props.spots === 1) {
+      return <h3 className="text--light">{props.spots} spot remaining</h3>
+    }
+     return <h3 className="text--light">{props.spots} spots remaining</h3>
+  }
+
+  let dayClass = classNames("li",{
+    "day-list__item": props,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <FormatSpots />
+    </li>
+  );
+}
