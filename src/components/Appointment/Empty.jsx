77da49bf925391cc7f8a610bfe87a7d08d40891f
Rui Dantas <ruidantas55@gmail.com>
2023-03-01 09:59:23 -0500

    Add the onAdd Story

--- src/components/Appointment/Empty.jsx
+++ src/components/Appointment/Empty.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function() {
+export default function(props) {
   return(
   <main className="appointment__add">
     <img
       className="appointment__add-button"
       src="images/add.png"
       alt="Add"
+      onClick={props.onAdd}
     />
   </main>
   )
